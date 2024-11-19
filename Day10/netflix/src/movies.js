76147c04034bbd348ea9c31b7ddce348bb463290
Sambhavgoel <sambhavgoelyash0907@gmail.com>
2024-11-19 15:37:11 +0530

    netflix-come

---
+++ Day10/netflix/src/movies.js
@@ -0,0 +1,67 @@
+const movies=[
+    {
+        id:1,
+        image: "https://rukminim2.flixcart.com/image/612/612/xif0q/movie/5/p/7/2009-dvd-eagle-hindi-tarzan-the-wonder-car-original-imahfevqrdeqzths.jpeg?q=70",
+        title : "Targen the Worderful Car",
+        language:"Hindi",
+        cost:200
+    },
+    {
+        id:2,
+        image: "https://rukminim2.flixcart.com/image/612/612/jo62kcw0/movie/q/m/k/2018-blu-ray-sony-pictures-excel-home-videos-english-ghost-rider-original-imafamrba4gfz3bw.jpeg?q=70",
+        title : "Ghost Rider",
+        language:"Hindi",
+        cost:200
+    },{
+        id:3,
+        image: "https://rukminim2.flixcart.com/image/612/612/av-media/movies/y/s/y/1947-earth-original-imadd6knfbpcycx7.jpeg?q=70",
+        title : "1947",
+        language:"Hindi",
+        cost:200
+    },{
+        id:4,
+        image: "https://rukminim2.flixcart.com/image/612/612/kph8h3k0/movie/w/x/l/2021-3d-blu-ray-warner-bros-excel-home-videos-english-justice-original-imag3ph6wufmqamk.jpeg?q=70",
+        title : "Justice League 3d",
+        language:"Hindi",
+        cost:200
+    },{
+        id:5,
+        image: "https://rukminim2.flixcart.com/image/612/612/k1wgx3k0/movie/a/e/f/2019-dvd-20th-century-fox-excel-home-videos-english-wayward-original-imafhd9abbdg4mp9.jpeg?q=70",
+        title : "Wayward pines",
+        language:"Hindi",
+        cost:200
+    },{
+        id:6,
+        image: "https://rukminim2.flixcart.com/image/612/612/kl9rssw0/movie/h/2/g/2020-dvd-20th-century-fox-excel-home-videos-english-titanic-original-imagyfz7ct8mqjpw.jpeg?q=70",
+        title : "Titanic",
+        language:"Hindi",
+        cost:200
+    },
+    {
+        id:7,
+        image: "https://rukminim2.flixcart.com/image/612/612/kydb3ww0/movie/r/f/l/2021-blu-ray-paramount-pictures-excel-home-videos-english-the-original-imagam39karzkfhh.jpeg?q=70",
+        title : "Jack Rayan",
+        language:"Hindi",
+        cost:200
+    },{
+        id:8,
+        image: "https://rukminim2.flixcart.com/image/612/612/ku5ufm80/movie/2/o/3/2020-3d-blu-ray-warner-bros-excel-home-videos-english-gravity-original-imag7ca8jfgyqm6n.jpeg?q=70",
+        title : "Gravity 3D",
+        language:"Hindi",
+        cost:200
+    },{
+        id:9,
+        image: "https://rukminim2.flixcart.com/image/612/612/jt7jhjk0/movie/z/f/z/2019-blu-ray-warner-bros-excel-home-videos-english-fantastic-original-imafehtpbxe49yw6.jpeg?q=70",
+        title : "Fantastic Beats",
+        language:"Hindi",
+        cost:200
+    },{
+        id:10,
+        image: "https://rukminim2.flixcart.com/image/612/612/ku5ufm80/movie/p/k/g/2020-3d-blu-ray-sony-pictures-excel-home-videos-english-spider-original-imag7ca8xv5adfzf.jpeg?q=70",
+        title : "Spiderman 3D",
+        language:"Hindi",
+        cost:200
+    },
+    
+];
+export default movies;
